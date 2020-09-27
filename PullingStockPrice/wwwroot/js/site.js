c672f17f55bf117a6f82bbe00a1ba0b6c58b1c0b
Michael Keaveny <michaelkeaveny0@gmail.com>
2020-09-27 22:48:26 +0100

    Displays intraday 1-minute interval data now

--- PullingStockPrice/wwwroot/js/site.js
+++ PullingStockPrice/wwwroot/js/site.js
@@ -2,7 +2,8 @@
     el: '#app',
     data: {
         high: '',
-        low: ''
+        low: '',
+        currentPrice: ''
     },
     methods: {
         getDailyStockPrice: async function () {
@@ -14,17 +15,17 @@
             const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${company}&outputsize=compact&apikey=51WURQPBUP1HGNHB`, { mode: 'cors' })
             const stockData = await response.json();
             console.log(stockData);
-            var yesterdayDate = this.getYesterdayDate().toString();
+            var yesterdayDate = this.getCurrentDay(1).toString();
             var yesterdayData = stockData["Time Series (Daily)"][yesterdayDate]
             // Condition to check if the current date is a Sunday, therefore gets the high and low from the Friday before the markets closed.
             if (typeof yesterdayData != 'string') {
-                yesterdayDate = this.getYesterdayDate(1).toString();
+                yesterdayDate = this.getCurrentDay(2).toString();
                 yesterdayData = stockData["Time Series (Daily)"][yesterdayDate]
                 console.log(yesterdayData);
             }
             // Condition to check if the current day of week is a Monday, therefore gets the high and low from the Friday before the markets closed.
             if (typeof yesterdayData != 'string') {
-                yesterdayDate = this.getYesterdayDate(2).toString();
+                yesterdayDate = this.getCurrentDay(3).toString();
                 yesterdayData = stockData["Time Series (Daily)"][yesterdayDate]
                 console.log(yesterdayData);
             }
@@ -35,7 +36,7 @@
             this.high = yesterdayHigh;
             this.low = yesterdayLow;
         },
-        getYesterdayDate: function (shiftDay = 0) {
+        getCurrentDay: function (shiftDay = 0) {
             var today = new Date();
             // Just in case the month is between 10 and 12
             var currentMonth = today.getMonth();
@@ -47,8 +48,54 @@
             else {
                 currentMonth = currentMonth.toString();
             }
-            var today = today.getFullYear() + '-' + currentMonth + '-' + (today.getDate() - 1 - shiftDay);
+            var today = today.getFullYear() + '-' + currentMonth + '-' + (today.getDate() - shiftDay);
             return today;
+        },
+        getCurrentStockPrice: async function () {
+            event.preventDefault();
+            const company = document.getElementById('current-price-input').value;
+            // Intraday API call, gets the value of every minute of trading.
+            const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${company}&interval=1min&outputsize=full&apikey=51WURQPBUP1HGNHB`, { mode: 'cors' })
+            const stockData = await response.json();
+            console.log(stockData);
+            var currentDate = this.getCurrentDay().toString();
+            console.log(currentDate);
+            // Get the current time in hours and minutes to work out the current market price.
+            var currentTime = this.getCurrentTime().toString();
+            console.log(currentDate + " " + currentTime + ":00");
+            var currentMarketPrice = stockData["Time Series (1min)"][currentDate + " " + currentTime + ":00"];
+            // var currentMarketPrice = stockData["Time Series (1min)"]["2020-09-25 16:00:00"]["1. open"];
+
+            // If the returned object is of type undefined thent the market must be closed.
+            if (typeof currentMarketPrice != 'string') {
+                this.currentPrice = "MARKET IS CLOSED";
+            }
+            // Else set the currentPrice to the currentMarketPrice.
+            else {
+                this.currentPrice = currentMarketPrice;
+            }
+        },
+        getCurrentTime: function () {
+            var today = new Date();
+            var hour = today.getHours();
+            var minutes = today.getMinutes();
+
+            if (hour < 10) {
+                hour = hour.toString();
+                hour = '0' + hour;
+            }
+            else {
+                hour = hour.toString();
+            }
+
+            if (minutes < 10) {
+                minutes = minutes.toString();
+                minutes = '0' + minutes;
+            }
+            else {
+                minutes = minutes.toString();
+            }
+            return hour + ":" + minutes;
         }
     }
 });
